7e9c9d38eb33055d64a93103eefdd6578aa424fb
Theo Cushion <theo@jivatechnology.com>
2008-10-28 17:15:25 +0000

    Initial import from http://code.nullstyle.com/git/railmail

---
+++ lib/railmail.rb
@@ -0,0 +1,19 @@
+require 'railmail/action_mailer'
+
+ActionMailer::Base.class_eval do
+  include Railmail::ActionMailer::InstanceMethods
+end
+
+module Railmail
+  def self.init(config, directory)
+    config.controller_paths << File.join(directory, 'app', 'controllers')
+    $LOAD_PATH << File.join(directory, 'app', 'controllers')
+    $LOAD_PATH << File.join(directory, 'app', 'models')
+    $LOAD_PATH << File.join(directory, 'app', 'helpers')
+    
+    Dependencies.load_paths << File.join(directory, 'app', 'controllers')
+    Dependencies.load_paths << File.join(directory, 'app', 'models')
+    Dependencies.load_paths << File.join(directory, 'app', 'helpers')
+  end
+end
+
