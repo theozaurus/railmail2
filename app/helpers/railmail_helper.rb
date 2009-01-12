8ffa0eb1cb3b57725d820fc667258ecaeb2af428
Theo Cushion <theo@jivatechnology.com>
2009-01-12 16:59:33 +0000

    Fix: Railmail now supports multipart emails with only a text/plain part present

--- app/helpers/railmail_helper.rb
+++ app/helpers/railmail_helper.rb
@@ -4,9 +4,11 @@ module RailmailHelper
     raw = d.raw
     return raw.body unless raw.multipart?
     
-    part = raw.parts.select {|p| p.content_type == 'text/html'}.first.body
-    part ||= "<pre class=\"plain_text\">#{raw.parts.select {|p| p.content_type == 'text/plain'}.first.body}</pre>"
-    part ||= raw.parts[0].body
-    part
+    html_parts = raw.parts.select {|p| p.content_type == 'text/html'}.map {|p| p = p.body}
+    plain_parts = raw.parts.select {|p| p.content_type == 'text/plain'}.map {|p| p = %(<pre class="plain_text">) + p.body + %(</pre>)}
+    other_parts = (raw.parts - html_parts - plain_parts).map {|p| p.body }
+    
+    part = html_parts.first || plain_parts.first || other_parts.first
+    
   end
 end
